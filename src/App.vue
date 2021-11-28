9d1eba19d4039c4d33b60e75ff45654cef47a09e
AlexOdnov <alex.odnoralov@yandex.ru>
2021-11-29 01:33:00 +0300

    update lib table

--- src/App.vue
+++ src/App.vue
@@ -6,74 +6,25 @@
         Custom
       </b-radio>
     </div>
-    <b-table
-      v-if="radio === 'libs'"
-      :data="data"
-      :columns="columns"
-      pagination-size="is-small"
-      per-page="10"
-      paginated
-      pagination-rounded
-      sort-multiple
-    />
-    <custom-table v-else :rows="data" />
+    <lib-table v-if="radio === 'libs'" />
+    <custom-table v-else virtScroll />
   </div>
 </template>
 
 <script>
 import CustomTable from './components/custom-table';
+import LibTable from './components/lib-table';
 
 export default {
   name: 'App',
   components: {
     CustomTable,
+    LibTable,
   },
   data() {
     return {
       radio: 'libs',
-      data: [],
-      columns: [
-        {
-          label: 'Post ID',
-          field: 'postId',
-          searchable: true,
-          sortable: true,
-          width: '10%',
-        },
-        {
-          label: 'ID',
-          field: 'id',
-          searchable: true,
-          sortable: true,
-          width: '10%',
-        },
-        {
-          label: 'Name',
-          field: 'name',
-          searchable: true,
-          sortable: true,
-          width: '25%',
-        },
-        {
-          label: 'Email',
-          field: 'email',
-          searchable: true,
-          sortable: true,
-          width: '25%',
-        },
-        {
-          label: 'Content',
-          field: 'body',
-          searchable: true,
-          sortable: true,
-          width: '30%',
-        },
-      ],
     };
-  },
-  async created() {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/comments`);
-    this.data = await res.json();
   },
 };
 </script>
